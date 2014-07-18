48899ea637717a49429e80460c26433821901e88
tanmay3011 <tanmay3011@gmail.com>
2014-07-18 20:40:25 +0530

    Add each loop instead of upto

--- occurence/lib/string.rb
+++ occurence/lib/string.rb
@@ -2,12 +2,10 @@ class String
 
   def count_alphabet_occurence
     alphabets_count = Hash.new(0)
-    #to split the input into individual characters
+    #to split the input into individual characters (used split instead of scan because regex comparisson are slower)
     alphabet_list = split(//)
-    0.upto(alphabet_list.length - 1) do |i|
-      #to increment that alphabets count
-      alphabets_count[alphabet_list[i]] += 1
-    end
+    #to increment that alphabets count
+    alphabet_list.each { |alphabet| alphabets_count[alphabet] += 1 }
     #return alphabet count
     alphabets_count
   end
