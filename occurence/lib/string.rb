c4c6bdba0058e4a48f35c0e338d892de4f419d92
tanmay3011 <tanmay3011@gmail.com>
2014-07-19 10:59:21 +0530

    Remove extra variable

--- occurence/lib/string.rb
+++ occurence/lib/string.rb
@@ -3,9 +3,8 @@ class String
   def count_alphabet_occurence
     alphabets_count = Hash.new(0)
     #to split the input into individual characters (used split instead of scan because regex comparisson are slower)
-    alphabet_list = split(//)
     #to increment that alphabets count
-    alphabet_list.each { |alphabet| alphabets_count[alphabet] += 1 }
+    split('').each { |alphabet| alphabets_count[alphabet] += 1 }
     #return alphabet count
     alphabets_count
   end
