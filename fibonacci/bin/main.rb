41bdc8bb4b5898eb1c9d5784d54fd3250c08d415
tanmay3011 <tanmay3011@gmail.com>
2014-07-21 21:08:25 +0530

    Correct indentation issue

--- fibonacci/bin/main.rb
+++ fibonacci/bin/main.rb
@@ -2,4 +2,4 @@ require_relative '../lib/fibonacci.rb'
 
 fibonacci = Fibonacci.new
 puts 'Fibonacci Series upto 1000 : '
-puts fibonacci.series {|operand1, operand2| [operand1, operand2]}
+puts fibonacci.series { |operand1, operand2| [operand1, operand2] }
