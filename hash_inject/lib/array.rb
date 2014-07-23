229caad0cf344d59e52917728bed7c29d3da2282
tanmay3011 <tanmay3011@gmail.com>
2014-07-23 19:14:53 +0530

    Initial Commit - Solution to exercise Hash-Inject 1. Add array.rb library to lib folder 2. Add main.rb to bin folder

---
+++ hash_inject/lib/array.rb
@@ -0,0 +1,18 @@
+class Array
+
+  def group_by_length
+    hash = Hash.new { |hash, key| hash[key] = [] }
+    for element in self
+      hash[element.to_s.length].push(element)
+    end
+    hash
+  end
+
+  def group_hash_by_even_odd_length
+    resultant_hash = Hash.new { |hash, key| hash[key] = [] }
+    group_by_length.inject(0) { |hash, (key, element)|
+      resultant_hash[key.even? ? 'even' : 'odd'].push(element)
+    }
+    resultant_hash
+  end
+end
