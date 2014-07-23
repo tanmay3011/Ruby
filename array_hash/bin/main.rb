6fc9573a91c5948a6100fc293a20b5efe3d45463
tanmay3011 <tanmay3011@gmail.com>
2014-07-23 16:30:56 +0530

    1. Remove function to_hash from array.rb library(not needed) 2. change the name of method call from to_hash to group_by_length

--- array_hash/bin/main.rb
+++ array_hash/bin/main.rb
@@ -1,3 +1,3 @@
 require_relative '../lib/array.rb'
 
-puts ['abc', 'def', 1234, 234, 'abcd', 'x', 'mnop', 5, 'zZzZ', nil].to_hash
+puts ['abc', 'def', 1234, 234, 'abcd', 'x', 'mnop', 5, 'zZzZ', nil].group_by_length
--- array_hash/lib/array.rb
+++ array_hash/lib/array.rb
@@ -1,15 +1,10 @@
 class Array
 
-  def to_hash
+  def group_by_length
     resultant_hash = Hash.new { |hash, key| hash[key] = [] }
-    group_by_length resultant_hash
-  end
-
-  def group_by_length(resultant_hash)
     for element in self
       resultant_hash[element.to_s.length].push(element)
     end
     resultant_hash
   end
-
 end
