8ff40a0d635143effbb298a5839a27bfc1125347
tanmay3011 <tanmay3011@gmail.com>
2014-07-24 12:50:04 +0530

    1. Use symbols instead of strings 2. Change name of class

--- hash_inject/bin/main.rb
+++ hash_inject/bin/main.rb
@@ -1,3 +1,3 @@
 require_relative '../lib/array'
 
-puts ['abc', 'def', 1234, 234, 'abcd', 'x', 'mnop', 5, 'zZzZ', nil].group_hash_by_even_odd_length
+puts ['abc', 'def', 1234, 234, 'abcd', 'x', 'mnop', 5, 'zZzZ', nil].group_by_even_odd_length
--- hash_inject/lib/array.rb
+++ hash_inject/lib/array.rb
@@ -8,10 +8,10 @@ class Array
     hash
   end
 
-  def group_hash_by_even_odd_length
+  def group_by_even_odd_length
     resultant_hash = Hash.new { |hash, key| hash[key] = [] }
     group_by_length.inject(0) { |hash, (key, element)|
-      resultant_hash[key.even? ? 'even' : 'odd'].push(element)
+      resultant_hash[key.even? ? :even : :odd].push(element)
     }
     resultant_hash
   end
