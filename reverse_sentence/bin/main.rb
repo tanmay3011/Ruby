a683bf47088a30de02c02c604c0272496bc038af
tanmay3011 <tanmay3011@gmail.com>
2014-07-26 12:46:50 +0530

    Initial Commit - Solution for reverse sentence exercise 1. Add string.rb library to lib folder 2. Add main.rb to bin folder

---
+++ reverse_sentence/bin/main.rb
@@ -0,0 +1,3 @@
+require_relative '../lib/string'
+
+puts 'An apple a day keeps the doctor away'.reverse_order_of_words
---
+++ reverse_sentence/lib/string.rb
@@ -0,0 +1,6 @@
+class String
+
+  def reverse_order_of_words
+    split.reverse!.join(' ')
+  end
+end
