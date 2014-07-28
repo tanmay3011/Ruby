7fccb3ffdfcd73c1bfb3911dee857d7da7ba43a3
tanmay3011 <tanmay3011@gmail.com>
2014-07-28 10:01:08 +0530

    Remove = from factorial method since 1 doesn't need to be multiplied Modify loop in pascal create to self + 1 to generate proper output

--- pascal_yield/lib/fixnum.rb
+++ pascal_yield/lib/fixnum.rb
@@ -1,11 +1,11 @@
 class Fixnum
 
   def factorial(number)
-    number >= 1 ? number * factorial(number - 1) : 1
+    number > 1 ? number * factorial(number - 1) : 1
   end
 
   def pascal_triangle_create
-    times do |i|
+    (self + 1).times do |i|
       (i+1).times do |j|
         yield factorial(i)/(factorial(j)*factorial(i-j))
         yield " "
