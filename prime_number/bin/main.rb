af7c5d7036e470a48a05598bc330212bb935f037
tanmay3011 <tanmay3011@gmail.com>
2014-07-28 11:37:52 +0530

    Loop modified(Not checking 1.prime?)

--- prime_number/bin/main.rb
+++ prime_number/bin/main.rb
@@ -1,4 +1,4 @@
 require_relative '../lib/fixnum'
 
-number = 25
+number = 0
 puts "List of prime numbers till #{ number } (inclusive): #{ number.generate_prime_numbers }"
--- prime_number/lib/fixnum.rb
+++ prime_number/lib/fixnum.rb
@@ -4,7 +4,8 @@ class Fixnum
 
   def generate_prime_numbers
     list_of_prime_numbers = []
-    1.step(self) { |number| list_of_prime_numbers.push(number) if number.prime? }
+    list_of_prime_numbers.push(2) if self > 1
+    (3..self).step(2) { |number| list_of_prime_numbers.push(number) if number.prime? }
     list_of_prime_numbers
   end
 end
