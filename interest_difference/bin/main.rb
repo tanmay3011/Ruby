d930130d7304ac155816717f5cd3d75127c13ffc
tanmay3011 <tanmay3011@gmail.com>
2014-07-30 02:38:56 +0530

    Add condition for principal and time to be greater than zero

--- interest_difference/bin/main.rb
+++ interest_difference/bin/main.rb
@@ -2,9 +2,9 @@ require_relative '../lib/interest'
 
 interest = Interest.new do |interest|
   puts 'Enter principal'
-  interest.principal = gets.chomp.to_f
+  interest.principal = gets.chomp while(interest.principal = interest.principal.to_f) <= 0
   puts 'Enter number of years for which interest is to be calculated'
-  interest.time = gets.chomp.to_i
+  interest.time = gets.chomp while(interest.time = interest.time.to_f) <= 0
   interest.rate = 100
 end
 
