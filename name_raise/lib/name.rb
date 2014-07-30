e6795bdf84f597a0010e5c97e2ce9ae7634c063b
tanmay3011 <tanmay3011@gmail.com>
2014-07-30 14:38:17 +0530

    Move Regex to constants

--- name_raise/lib/name.rb
+++ name_raise/lib/name.rb
@@ -4,13 +4,15 @@ require_relative '../lib/case_error'
 class Name
 
   attr_reader :firstname, :lastname
+  REGEX_FOR_EMPTY_STRING = /^\s*$/
+  REGEX_FOR_FIRST_LETTER_CAPITAL = /[A-Z]/
   def initialize(firstname, lastname)
     begin
-      raise NullInputError, 'First name not given' if !(firstname !~ /^\s*$/)
-      raise CaseError, 'First letter of First name is not capital' if firstname[0] !~ /[A-Z]/
+      raise NullInputError, 'First name not given' if !(firstname !~ REGEX_FOR_EMPTY_STRING)
+      raise CaseError, 'First letter of First name is not capital' if firstname[0] !~ REGEX_FOR_FIRST_LETTER_CAPITAL
       @firstname = firstname
 
-      raise NullInputError, 'Last name not given' if !(lastname !~ /^\s*$/)
+      raise NullInputError, 'Last name not given' if !(lastname !~ REGEX_FOR_EMPTY_STRING)
       @lastname = lastname
     end
   end
