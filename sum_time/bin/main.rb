086165b459f38837aaa7531ca38af59d5871bd60
tanmay3011 <tanmay3011@gmail.com>
2014-07-31 01:25:39 +0530

    1. Change method name to add instead of add_time 2. Correct Regex for time over 24 60 3. Use :all? method instead of each 4. Store initial time as Base Time(constant)

--- sum_time/bin/main.rb
+++ sum_time/bin/main.rb
@@ -1,9 +1,5 @@
 require_relative '../lib/time'
 
-time1 = "1:45:34"
-time2 = "2:15:58"
-time_first = Time.parse(time1) if Time.valid?(time1)
-time_second = Time.parse(time2) if Time.valid?(time2)
-added_time = time_first.add_time(time_second)
+added_time = Time.add("3:45:14", "02:15:58")
 print "#{ added_time[:days] } Day & " if !added_time[:days].zero?
-puts "#{ added_time[:hours] } : #{ added_time[:mins] } : #{ added_time[:secs] } \n"
+puts "#{ added_time[:hours] } : #{ added_time[:mins] } : #{ added_time[:secs] }"
--- sum_time/lib/time.rb
+++ sum_time/lib/time.rb
@@ -1,13 +1,19 @@
 require "time"
 class Time
 
-  REGEX_TIME = /([01]?[0-9]|2[0-3]):([0-5][0-9]|6[0]):([0-5][0-9]|6[0])/
-  def self.valid?(time)
-    time =~ REGEX_TIME
+  REGEX_TIME = /^([0-1]?[0-9]|2[0-3]):([0-5]?[0-9]|6[0]):([0-5]?[0-9]|6[0])$/
+  BASE_TIME = Time.parse("00:00:00")
+
+  def self.valid?(*time)
+    time.all? { |time| time =~ REGEX_TIME }
   end
 
-  def add_time(time)
-    new_time = self + (time - Time.parse("00:00:00"))
+  def self.add(*time_arguments)
+    abort("Wrong time format") if !(valid?(*time_arguments))
+    new_time = BASE_TIME
+    time_arguments.each do |time|
+      new_time += (Time.parse(time) - BASE_TIME)
+    end
     days = (Time.now.day == new_time.day) ? 0 : 1
     return({ days: days, hours: new_time.hour, mins: new_time.min, secs: new_time.sec })
   end
