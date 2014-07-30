2ccb748a044739691dfe38cf0d756c1c1f114a44
tanmay3011 <tanmay3011@gmail.com>
2014-07-30 21:07:26 +0530

    Initial Commit: Solution for CSV exercise

---
+++ csv/lib/csv_data.rb
@@ -0,0 +1,14 @@
+class CsvData
+
+  attr_reader :name, :employee_id
+  @@csv_path =
+
+  def initialize(name, employee_id)
+    @name = name
+    @employee_id = employee_id
+  end
+
+  def to_s
+    "#{ name } (EmpId: #{ employee_id }) "
+  end
+end
