c89e143fee65638fa07a17506a7023335fcb9673
tanmay3011 <tanmay3011@gmail.com>
2014-07-31 01:47:53 +0530

    Use NegativeNumberError instead of StandardError Add rescue operation to main program with raise checking hwether negative number or not

--- factorial_exception/bin/main.rb
+++ factorial_exception/bin/main.rb
@@ -1,9 +1,4 @@
 require_relative '../lib/fixnum'
 
 number = -4
-begin
-  raise NegativeNumberError if number < 0
-  puts "Factorial of #{ number } is : #{ number.factorial }"
-rescue StandardError
-  puts "#{ $! } : Factorial is not defined"
-end
+puts "Factorial of #{ number } is : #{ number.factorial }"
--- factorial_exception/lib/fixnum.rb
+++ factorial_exception/lib/fixnum.rb
@@ -3,10 +3,16 @@ require_relative '../lib/negative_number_error'
 class Fixnum
 
   def factorial
-    if self >= 1
-      (1..self).inject(:*)
+    if self < 0
+      raise NegativeNumberError
     elsif zero?
       1
+    else
+      (1..self).inject(:*)
     end
+
+    rescue NegativeNumberError
+      puts "#{ $! }: Negative Number is not allowed thus Factorial is not defined"
+      abort
   end
 end
