d2d1cd00b3f14b3f3a052b8d30e660321214fe14
tanmay3011 <tanmay3011@gmail.com>
2014-07-31 01:15:51 +0530

    1. Change method name to add instead of add_time 2. Correct Regex for time over 24 60 3. Use all? iterator instead of each 4. Store initial time as Base_Time(constant)

--- sum_time_multiple/bin/main.rb
+++ sum_time_multiple/bin/main.rb
@@ -1,5 +1,5 @@
 require_relative '../lib/time'
 
-added_time = Time.add_time("11:23:07", "22:53:45", "0:23:23", "23:45:56")
+added_time = Time.add("23:23:45", "22:03:45", "0:23:23", "23:45:56")
 print "#{ added_time[:days] } Days & " if !added_time[:days].zero?
-puts "#{ added_time[:hours] } : #{ added_time[:mins] } : #{ added_time[:secs] } \n"
+puts "#{ added_time[:hours] } : #{ added_time[:mins] } : #{ added_time[:secs] }"
