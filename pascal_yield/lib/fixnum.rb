71ef013cda4f1fb79c567d6882e0ce7afe1f7921
tanmay3011 <tanmay3011@gmail.com>
2014-07-31 00:34:50 +0530

    Add separate function for combination Optimize logic for combinations

--- pascal_yield/lib/fixnum.rb
+++ pascal_yield/lib/fixnum.rb
@@ -1,18 +1,13 @@
 class Fixnum
 
-  def factorial
-    #if whole_number? only in case if we need this function from outside
-    #else this method should be private and remove whole_number? method from here
-    if whole_number?
-      self > 1 ? self * (self - 1).factorial : 1
-    end
-  end
-
   def create_pascal_triangle
     if whole_number?
       (self + 1).times do |i|
         (i + 1).times do |j|
-          yield i.factorial / (j.factorial * (i - j).factorial)
+          if j > i / 2
+            j = i - j
+          end
+          yield combination(i, j)
           yield " "
         end
         yield "\n"
@@ -20,8 +15,18 @@ class Fixnum
     end
   end
 
+  def factorial
+    if whole_number?
+      self > 1 ? self * (self - 1).factorial : 1
+    end
+  end
+
   private
     def whole_number?
       self >= 0
+    end
+
+    def combination(i, j)
+      i.factorial / (j.factorial * (i - j).factorial)
     end
 end
