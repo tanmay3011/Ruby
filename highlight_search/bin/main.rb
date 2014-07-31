309f67109e7e5ce71225a074317fb1edd912ce55
tanmay3011 <tanmay3011@gmail.com>
2014-07-31 13:14:42 +0530

    change variable name use one method instead of two

--- highlight_search/bin/main.rb
+++ highlight_search/bin/main.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/string'
 
 puts 'Enter the string:'
-string_to_be_searched = gets.chomp
+sentence = gets.chomp
 
 puts 'Enter a word that needs to highlighted :'
-puts string_to_be_searched.highlight_search_result(gets.chomp)
+puts sentence.highlight_search_result(gets.chomp)
--- highlight_search/lib/string.rb
+++ highlight_search/lib/string.rb
@@ -1,7 +1,12 @@
 class String
 
   def highlight_search_result(word_to_be_searched)
+    occurences = 0
     regex = Regexp.new("(#{ word_to_be_searched })", 'i')
-    "#{ gsub(regex, '(\\1)') } \nTotal occurrences: #{ scan(regex).length }"
+    gsub!(regex) do |matched_word|
+      occurences += 1
+      "(#{ matched_word })"
+    end
+    "#{ self }\nTotal occurrences: #{ occurences }"
   end
 end
