77b06f8b715ace9ed536d6d9b2199bbb0c0be9ac
tanmay3011 <tanmay3011@gmail.com>
2014-08-04 01:21:04 +0530

    Initial Commit: Solution for Sales Tax Exercise

---
+++ sales_tax/bin/main.rb
@@ -0,0 +1,6 @@
+require_relative '../lib/order'
+
+order = Order.new
+order.add_item { |message| puts message; gets.chomp }
+puts "Product  Price"
+order.generate_bill { |output_message| puts output_message }
