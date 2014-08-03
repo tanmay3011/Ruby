45ed01e7bf8cb6f97f911bd6da1aa32b956db465
tanmay3011 <tanmay3011@gmail.com>
2014-08-04 01:08:31 +0530

    Remove files

--- sales_tax_problem/bin/main.rb
+++
@@ -1,6 +0,0 @@
-require_relative '../lib/order'
-
-order = Order.new()
-order.add_item { |message| puts message; gets.chomp }
-puts "Product  Price"
-order.generate_bill { |output_message| puts output_message }
